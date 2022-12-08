dfd1d57dae86ffb2e511b2a02fd161adaf85df66
RAFAT <rafatalbadri@yahoo.com>
2022-12-08 19:57:01 +0100

    no action bar+ start menu activity_game.xml/GameActivity.kt + landscape mode .

--- app/src/main/java/com/example/classicponggame/InGameActivity.kt
+++ app/src/main/java/com/example/classicponggame/InGameActivity.kt
@@ -3,7 +3,7 @@ package com.example.classicponggame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class InGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(GameView(this))
